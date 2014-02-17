70fd8b85f7ed25939bb9e372bd911405f16ef545
Gareth Pulham <garethpulham@gmail.com>
2014-02-17 12:27:24 +0000

    Update gitignore for atmel studio and begin the bootloader section

---
+++ bootloader/bootloader.c
@@ -0,0 +1,15 @@
+/*
+ * bootloader.c
+ *
+ * Created: 17/02/2014 12:06:24
+ *  Author: Gareth
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void){
+    for(;;){
+        //TODO:: Please write your application code 
+    }
+}
